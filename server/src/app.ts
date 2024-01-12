270dd521d8709e347d7fe2415b96a72abfe1712d
Julian Lee <julianlee@julians-mbp-2.myfiosgateway.com>
2024-01-12 17:21:52 -0500

    CAS authentication, js->ts

---
+++ server/src/app.ts
@@ -0,0 +1,37 @@
+import cors from "cors";
+import express from "express";
+import session from "express-session";
+import { isCI, isDevelopment, isTest } from "./utils/environment";
+import passport from "./passport";
+import routes from "./routes";
+
+const bypassCors = isCI() || isDevelopment() || isTest();
+const allowList = new Set(["http://localhost:3000"]);
+
+const corsOptions = {
+  origin: (origin: string, callback: any) => {
+    if (bypassCors || allowList.has(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+};
+
+const app = express()
+  .use(cors(corsOptions))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(
+    session({
+      secret: "this is totally secret",
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+  .use(routes);
+
+passport(app);
+
+export default app;
