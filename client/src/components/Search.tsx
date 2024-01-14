16e2b50fb8590d8d5e15b844abf22955212531e5
Julian Lee <julianlee@julians-mbp-2.myfiosgateway.com>
2024-01-14 13:16:01 -0500

    Dynamic listing lookup

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,61 +1,54 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Autocomplete from '@mui/material/Autocomplete';
 import Stack from '@mui/material/Stack';
 import {Listing} from '../types/types';
 import {departmentNames} from '../utils/departmentNames'; 
+import axios from 'axios';
  
 type SearchProps = {
   setListings: (listings: Listing[]) => void;
 }
 
-function createListing(
-  id: number,
-  departments: string,
-  email: string,
-  website: string,
-  description: string,
-  keywords: string,
-  lastUpdated: string,
-  name: string,
-): Listing {
-  return {
-    id,
-    departments,
-    email,
-    website,
-    description,
-    keywords,
-    lastUpdated,
-    name
-  };
-}
-
-export const sampleListings = [
-  createListing(5, 'American Studies, African American Studies', 'test@yale.edu', 'www.yale.edu', 'description', 'keyword', '2017-09-29 19:27:48', 'John Doe'),
-  createListing(6, 'English', 'test@yale.edu', 'www.yale.edu', 'description2', 'keyword2', '2016-09-29 19:27:48', 'Jane Doe'),
-];
-
 export default function Search(props: SearchProps) {
     const {setListings} = props;
-    const [lastNamePI, setLastNamePI] = useState("");
-    const [keywords, setKeywords] = useState("");
+    const [lastNamePI, setLastNamePI] = useState('');
+    const [keywords, setKeywords] = useState('');
     const [departments, setDepartments] = useState<string[]>([]); 
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => { 
       event.preventDefault();
       console.log(lastNamePI, keywords, departments);
-      setListings(sampleListings);
+      const url = 'http://localhost:4000/listings?dept=' + departments 
+                  + '&keywords=' + keywords + '&lname=' + lastNamePI
+      axios.get(url).then((response) => {
+        //console.log(response.data);
+        const responseListings : Listing[] = response.data.map(function(elem: any){
+            return {
+              id: elem._id,
+              departments: elem.departments,
+              email: elem.email,
+              website: elem.website,
+              description: elem.description,
+              keywords: elem.keywords,
+              lastUpdated: elem.last_updated,
+              name: elem.fname + ' ' + elem.lname
+            }
+        })
+        console.log(url);
+        setListings(responseListings);
+      });
+      //setListings(sampleListings);
     }
 
     return (
       <form onSubmit={handleSubmit} noValidate>     
-          <Stack direction="row" justifyContent="space-around" alignItems="flex-start" spacing={1.5}>
+          <Stack direction='row' justifyContent='space-around' alignItems='flex-start' spacing={1.5}>
                   <Autocomplete
                       multiple
                       limitTags={2}
-                      id="tags-outlined"
+                      id='tags-outlined'
                       options={departmentNames}
                       onChange={(event, newDept) => {
                           setDepartments(newDept);
@@ -66,25 +59,25 @@ export default function Search(props: SearchProps) {
                       <TextField
                           {...params}
                           required
-                          label="Departments"
-                          placeholder=""
+                          label='Departments'
+                          placeholder=''
                       />
                       )}
                   />
                   <TextField 
-                      id="professor-search" 
-                      label="PI Last Name" 
+                      id='professor-search' 
+                      label='PI Last Name' 
                       onChange={e => setLastNamePI(e.target.value)} 
-                      type="search" 
+                      type='search' 
                       sx={{width: '350px'}}/>
                   <TextField 
-                      id="keyword-search" 
-                      label="Keyword(s) (comma-separated)" 
+                      id='keyword-search' 
+                      label='Keyword(s) (comma-separated)' 
                       onChange={e => setKeywords(e.target.value)} 
-                      type="search" 
+                      type='search' 
                       sx={{width: '350px'}}/>
                   <Button
-                      type="submit"
+                      type='submit'
                       sx={{paddingTop: '15px'}}
                   >
                       Search</Button>
