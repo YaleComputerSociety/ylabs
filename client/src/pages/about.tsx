fcc39e2f43e786bcc297846cf4a6ef6c1221ead4
Ryan Fernandes <ryan@fernandes.us>
2025-02-04 09:16:21 -0500

    YURA Button for login page

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,7 +1,9 @@
 const About = () => {
     return (
         <div style={{marginTop: '6rem', marginLeft: '3rem', marginRight: '3rem'}}>
-            <h1>This is the about page</h1>
+            <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-2xl">
+                About page
+            </div>
         </div>
     );
 };
