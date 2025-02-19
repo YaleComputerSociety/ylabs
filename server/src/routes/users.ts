656991ec37de97763a44cd333dfb646204ada6d0
Ryan Fernandes <ryan@fernandes.us>
2025-02-18 22:38:53 -0500

    Routes/services for editing user list data

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,10 @@
-import { createUser, readAllUsers, readUser, updateUser, deleteUser } from '../services/userService';
+import { createUser, readAllUsers, readUser, updateUser, deleteUser, addDepartments, deleteDepartments, clearDepartments, addOwnListings, deleteOwnListings, clearOwnListings, addFavListings, deleteFavListings, clearFavListings } from '../services/userService';
 import { NotFoundError } from "../utils/errors";
 import { Request, Response, Router } from "express";
 
 const router = Router();
+
+//User level routes
 
 //Create new user
 router.post("/", async (request: Request, response: Response) => {
@@ -70,5 +72,148 @@ router.delete('/:id', async (request: Request, response: Response) => {
         }
     }
 });
+
+//Departments level routes
+
+//Add departments by ObjectId or NetId
+router.put('/:id/departments', async (request: Request, response: Response) => {
+    try {
+        const user = await addDepartments(request.params.id, Array.isArray(request.body.departments) ? request.body.departments : [request.body.departments]);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Remove departments by ObjectId or NetId
+router.delete('/:id/departments', async (request: Request, response: Response) => {
+    try {
+        const user = await deleteDepartments(request.params.id, Array.isArray(request.body.departments) ? request.body.departments : [request.body.departments]);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Clear departments by ObjectId or NetId
+router.delete('/:id/departments/all', async (request: Request, response: Response) => {
+    try {
+        const user = await clearDepartments(request.params.id);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Own listings level routes
+
+//Add ownListings by ObjectId or NetId
+router.put('/:id/ownListings', async (request: Request, response: Response) => {
+    try {
+        const user = await addOwnListings(request.params.id, Array.isArray(request.body.ownListings) ? request.body.ownListings : [request.body.ownListings]);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Remove ownListings by ObjectId or NetId
+router.delete('/:id/ownListings', async (request: Request, response: Response) => {
+    try {
+        const user = await deleteOwnListings(request.params.id, Array.isArray(request.body.ownListings) ? request.body.ownListings : [request.body.ownListings]);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Clear ownListings by ObjectId or NetId
+router.delete('/:id/ownListings/all', async (request: Request, response: Response) => {
+    try {
+        const user = await clearOwnListings(request.params.id);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Fav listings level routes
+
+//Add favListings by ObjectId or NetId
+router.put('/:id/favListings', async (request: Request, response: Response) => {
+    try {
+        const user = await addFavListings(request.params.id, Array.isArray(request.body.favListings) ? request.body.favListings : [request.body.favListings]);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Remove favListings by ObjectId or NetId
+router.delete('/:id/favListings', async (request: Request, response: Response) => {
+    try {
+        const user = await deleteFavListings(request.params.id, Array.isArray(request.body.favListings) ? request.body.favListings : [request.body.favListings]);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Clear favListings by ObjectId or NetId
+router.delete('/:id/favListings/all', async (request: Request, response: Response) => {
+    try {
+        const user = await clearFavListings(request.params.id);
+        response.status(200).json({ user });
+    } catch (error) {
+        console.log(error.message);
+        if (error instanceof NotFoundError) {
+            response.status(error.status).json({ error: error.message });
+        } else {
+            response.status(500).json({ error: error.message });
+        }
+    }
+});
+
+//Handle login (/users/loginData/:id)
 
 export default router;
