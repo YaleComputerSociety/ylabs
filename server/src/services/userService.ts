df01f82998027e3c71debca01ae5d577d5995e47
Ryan Fernandes <ryan@fernandes.us>
2025-02-18 22:38:53 -0500

    Routes/services for editing user list data

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -4,22 +4,14 @@ import { createUserBackup, updateUserBackup, userBackupExists } from "./userBack
 import mongoose from "mongoose";
 
 export const createUser = async (userData: any) => {
-    try {
-        const user = new User(userData);
-        await user.save();
-        return user.toObject();
-    } catch (error) {
-        throw new Error(error.message);
-    }
+    const user = new User(userData);
+    await user.save();
+    return user.toObject();
 };
 
 export const readAllUsers = async () => {
-    try {
-        const users = await User.find();
-        return users.map(user => user.toObject());
-    } catch (error) {
-        throw new Error(error.message);
-    }
+    const users = await User.find();
+    return users.map(user => user.toObject());
 };
 
 export const readUser = async(id: any) => {
@@ -110,3 +102,95 @@ export const deleteUser = async(id: any) => {
         return user.toObject();
     }
 }
+
+//List data routes
+
+//Add departments
+export const addDepartments = async(id: any, newDepartments: [string]) => {
+    let user = await readUser(id);
+
+    user.departments.push(...newDepartments);
+    user.departments = Array.from(new Set(user.departments));
+
+    const newUser = await updateUser(id, {"departments": user.departments});
+
+    return newUser;
+};
+
+//Remove departments
+export const deleteDepartments = async(id: any, removedDepartments: [string]) => {
+    let user = await readUser(id);
+
+    user.departments = user.departments.filter(department => removedDepartments.indexOf(department) < 0);
+
+    const newUser = await updateUser(id, {"departments": user.departments});
+
+    return newUser;
+};
+
+//Clear departments
+export const clearDepartments = async(id: any) => {
+    const newUser = await updateUser(id, {"departments": []});
+
+    return newUser;
+};
+
+//Add own listings
+export const addOwnListings = async(id: any, newListings: [mongoose.Types.ObjectId]) => {
+    let user = await readUser(id);
+
+    user.ownListings.push(...newListings);
+    user.ownListings = Array.from(new Set(user.ownListings));
+
+    const newUser = await updateUser(id, {"ownListings": user.ownListings});
+
+    return newUser;
+};
+
+//Remove own listings
+export const deleteOwnListings = async(id: any, removedListings: [mongoose.Types.ObjectId]) => {
+    let user = await readUser(id);
+
+    user.ownListings = user.ownListings.filter(listing => removedListings.indexOf(listing) < 0);
+
+    const newUser = await updateUser(id, {"ownListings": user.ownListings});
+
+    return newUser;
+};
+
+//Clear own listings
+export const clearOwnListings = async(id: any) => {
+    const newUser = await updateUser(id, {"ownListings": []});
+
+    return newUser;
+};
+
+//Add fav listings
+export const addFavListings = async(id: any, newListings: [mongoose.Types.ObjectId]) => {
+    let user = await readUser(id);
+
+    user.favListings.push(...newListings);
+    user.favListings = Array.from(new Set(user.favListings));
+
+    const newUser = await updateUser(id, {"favListings": user.favListings});
+
+    return newUser;
+};
+
+//Remove fav listings
+export const deleteFavListings = async(id: any, removedListings: [mongoose.Types.ObjectId]) => {
+    let user = await readUser(id);
+
+    user.favListings = user.favListings.filter(listing => removedListings.indexOf(listing) < 0);
+
+    const newUser = await updateUser(id, {"favListings": user.favListings});
+
+    return newUser;
+};
+
+//Clear fav listings
+export const clearFavListings = async(id: any) => {
+    const newUser = await updateUser(id, {"favListings": []});
+
+    return newUser;
+};
