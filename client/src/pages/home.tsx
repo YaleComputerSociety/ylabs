b5c11f63f83518ed2fb8ff1adca044739e65db4a
Ryan Fernandes <ryan@fernandes.us>
2025-02-23 16:24:21 -0500

    First progress on search bar display

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,18 +1,30 @@
 import {useState} from "react";
 import ListingsTable from "../components/ListingsTable";
+
 import Search from "../components/Search";
+import SearchBar from "../components/search/SearchBar";
+import FilterDropdown from "../components/search/FilterDropdown";
+import { departmentNames } from "../utils/departmentNames";
+
 import styled from "styled-components";
 import {Listing} from '../types/types';
 import PulseLoader from "react-spinners/PulseLoader";
- 
+
+//<Search setListings={setListings} setIsLoading={setIsLoading} numSearches={numSearches} setNumSearches={setNumSearches}></Search>
+
 const Home = () => {
     const [listings, setListings] = useState<Listing[]>([]);
     const [numSearches, setNumSearches] = useState(0);
     const [isLoading, setIsLoading] = useState<Boolean>(false);
 
+    const [queryString, setQueryString] = useState<string>("");
+
     return (
         <div style={{marginTop: '6rem', marginLeft: '3rem', marginRight: '3rem'}}>
-            <Search setListings={setListings} setIsLoading={setIsLoading} numSearches={numSearches} setNumSearches={setNumSearches}></Search>
+            <SearchBar queryString={queryString} setQueryString={setQueryString}></SearchBar>
+            <div className='mt-12'>
+                <FilterDropdown allDepartments={departmentNames}></FilterDropdown>
+            </div>
             <div style={{marginTop: '2rem'}}></div>
             {isLoading ? (
                 <div style={{marginTop: '17%', textAlign: 'center'}}>
