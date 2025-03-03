74b2fed9f49e76fa64f5e8dda8cc4584a644b428
Ryan Fernandes <ryan@fernandes.us>
2025-03-02 23:02:34 -0500

    Frontend changes to fetch correct backend

--- client/src/components/SignInButton.tsx
+++ client/src/components/SignInButton.tsx
@@ -1,12 +1,18 @@
 import Button from "@mui/material/Button";
 
-const SignInButton = () => (
-  <Button
-    variant="contained"
-    href={process.env.REACT_APP_SERVER + `/cas?redirect=${window.location.origin}`}
-  >
-    Sign in With Yale CAS
-  </Button>
-);
+const SignInButton = () => {
+  const backendBaseURL = window.location.host.includes("yalelabs.io")
+    ? "https://yalelabs.io"
+    : process.env.REACT_APP_SERVER;
+  
+  return (
+    <Button
+      variant="contained"
+      href={backendBaseURL + `/cas?redirect=${window.location.origin}`}
+    >
+      Sign in With Yale CAS
+    </Button>
+  );
+};
 
 export default SignInButton;
