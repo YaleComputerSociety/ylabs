763bf96e619138ca6879148cced207bcffe0dddf
Ryan Fernandes <ryan@fernandes.us>
2025-03-04 19:17:09 -0500

    Dangerous routes hidden

--- server/src/routes/listings.ts
+++ server/src/routes/listings.ts
@@ -5,7 +5,10 @@ import { Request, Response, Router } from "express";
 const router = Router();
 
 //Create new listing
-router.post("/", async (request: Request, response: Response) => {
+
+//Hidden for security reasons
+
+/*router.post("/", async (request: Request, response: Response) => {
     try {
         const listing = new Listing(request.body);
         await listing.save();
@@ -14,7 +17,7 @@ router.post("/", async (request: Request, response: Response) => {
         console.log(error.message);
         response.status(400).json({ error: error.message, success: false });
     }
-});
+});*/
 
 // Route for getting listing by id: for testing
 router.get('/byId/:id', async (request: Request, response: Response) => {
