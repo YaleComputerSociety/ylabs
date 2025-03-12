49ce61dd4de324384ad16da71baaa52216d2803e
Ryan Fernandes <ryan@fernandes.us>
2025-03-11 20:25:04 -0400

    Reformatted to use NewListings model and added color bar hiringStatus indicator

--- client/src/types/types.tsx
+++ client/src/types/types.tsx
@@ -3,6 +3,25 @@
  */
 import type { LabScholarlyLink } from './labDetail';
 
+export type NewListing = {
+  id: number;
+  professorIds: string[];
+  professorNames: string[];
+  title: string;
+  departments: string[];
+  emails: string[];
+  websites: string[];
+  description: string;
+  keywords: string[];
+  established: string;
+  views: number;
+  favorites: number;
+  hiringStatus: number;
+  archived: boolean;
+  updatedAt: string;
+  createdAt: string;
+};
+
 export type Listing = {
   id: string;
   ownerId: string;
