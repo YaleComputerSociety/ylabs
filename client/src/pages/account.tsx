a8b4fd54fb7fe46c70fbc4bc4143c2650b246397
Ryan Fernandes <ryan@fernandes.us>
2025-03-17 11:41:38 -0400

    Added frontend support for display of user confirmation and hiding unconfirmed/archived listings in favorites

--- client/src/pages/account.tsx
+++ client/src/pages/account.tsx
@@ -137,6 +137,10 @@ const Account = () => {
         setFavListings((prevFavListings) => prevFavListings.map((listing) => listing.id === newListing.id ? newListing : listing));
     };
 
+    const filterHiddenListings = (listings: NewListing[]) => {
+        return listings.filter((listing) => listing.confirmed && !listing.archived);
+    }
+
     const postListing = async (newListing: NewListing) => {
         setIsLoading(true);
         axios.put(`/newListings/${newListing.id}`, {withCredentials: true, data: newListing}).then((response) => {
@@ -228,7 +232,17 @@ const Account = () => {
                     <PulseLoader color="#66CCFF" size={10} /> 
                 </div>
             ) : (
-                <div className="">
+                <div>
+                    {user && !user.userConfirmed && (
+                        <div className="bg-amber-100 border-l-4 border-amber-500 text-amber-700 p-4 mb-6 rounded shadow-sm">
+                            <div className="flex items-center">
+                                <svg className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                                </svg>
+                                <p className="font-medium">Your account is pending confirmation. Any listings that you create will not be publicly visible as favorites or in search results until your account is confirmed.</p>
+                            </div>
+                        </div>
+                    )}
                     <p className="text-xl text-gray-700 mb-4">Your listings</p>
                     {ownListings.length > 0 ? (
                         <ul>
@@ -261,9 +275,9 @@ const Account = () => {
                         </div>
                     )}
                     <p className="text-xl text-gray-700 mb-4">Favorite listings</p>
-                    {favListings.length > 0 ? (
+                    {filterHiddenListings(favListings).length > 0 ? (
                         <ul>
-                            {favListings.map((listing) => (
+                            {filterHiddenListings(favListings).map((listing) => (
                                 <li key={listing.id} className="mb-2">
                                     <ListingCard
                                         listing={listing}
