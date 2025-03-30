089349d9c2ba7acf5f32962e4f87558cfbe2223f
Ryan Fernandes <ryan@fernandes.us>
2025-03-30 19:19:01 -0400

    Changed login button from linking to YURA to just reloading page

--- client/src/components/YURAButton.tsx
+++ client/src/components/YURAButton.tsx
@@ -1,12 +1,13 @@
 import Button from "@mui/material/Button";
 
 const YURAButton = () => {
+    const handleReload = () => {
+        window.location.reload();
+    };
+
     return(
         <Button
-            component="a"
-            href="https://www.yura.yale.edu/"
-            target="_blank"
-            rel="noopener noreferrer"
+            onClick={handleReload}
         >
             <img src="/assets/logos/ylabs-temp.png" alt="ylab-logo" style={{width: '65px', height: '26px'}} />
         </Button>
