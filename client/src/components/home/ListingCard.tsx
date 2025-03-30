fd92a2787974b10c736a026a326bddd3ebe268d5
Ryan Fernandes <ryan@fernandes.us>
2025-03-30 19:06:54 -0400

    Home Page Updates
    * Progress on new search, backend route finalized with search index, working on integrating to home page, made temp, will need to restructure better
    
    * Listings displayed on home page in new format, fixed text overflow bug, adding sort button functionality (wip)
    
    * Sort button full functionality, loading-proof
    
    * Fixed sorting order for text vs. dates
    
    * Added favoriting functionality to cards and listings, fixed multiple search glitch on load
    
    * Filtered out unconfirmed and archived listings from search results
    
    * (Re)-added infinite scroll and added secondary sorting by _id to results
    
    * Authentication protection for new listings search
    
    * Fix get listings from search hub not including {withCredentials: true}

---
+++ client/src/components/home/ListingCard.tsx
@@ -0,0 +1,255 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { NewListing } from '../../types/types';
+import { departmentCategories } from '../../utils/departmentNames';
+import axios from "../../utils/axios";
+import swal from "sweetalert";
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext";
+
+interface ListingCardProps {
+    listing: NewListing;
+    favListingsIds: string[];
+    updateFavorite: (listingId: string, favorite: boolean) => void;
+    openModal: (listing: NewListing) => void;
+}
+
+const ListingCard = ({ listing, favListingsIds, updateFavorite, openModal }: ListingCardProps) => {
+    const [visibleDepartments, setVisibleDepartments] = useState<string[]>([]);
+    const [moreCount, setMoreCount] = useState(0);
+    const [showTooltip, setShowTooltip] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(favListingsIds.includes(listing.id));
+    const departmentsContainerRef = useRef<HTMLDivElement>(null);
+    const {user} = useContext(UserContext);
+
+    const departmentColors = [
+        "bg-blue-200",
+        "bg-green-200",
+        "bg-yellow-200",
+        "bg-red-200",
+        "bg-purple-200",
+        "bg-pink-200",
+        "bg-teal-200",
+        "bg-orange-200"
+    ];
+
+    // Helper function to determine bar color based on hiringStatus
+    const getHiringStatusColor = () => {
+        if (listing.hiringStatus < 0) {
+            return "bg-red-500";
+        } else if (listing.hiringStatus === 0) {
+            return "bg-yellow-500";
+        } else {
+            return "bg-green-500";
+        }
+    };
+    
+    // Helper function to get tooltip text based on hiring status
+    const getHiringStatusText = () => {
+        if (listing.hiringStatus < 0) {
+            return "Lab not seeking applicants";
+        } else if (listing.hiringStatus === 0) {
+            return "Lab open to applicants";
+        } else {
+            return "Lab seeking applicants";
+        }
+    };
+
+    useEffect(() => {
+        // Set listing as favorite based on if listing.id is in favListingsIds
+        if(favListingsIds) {
+            setIsFavorite(favListingsIds.includes(listing.id));
+        }
+    }, [favListingsIds]);
+
+    useEffect(() => {
+        if (!departmentsContainerRef.current) return;
+        
+        const calculateVisibleDepartments = () => {
+            const container = departmentsContainerRef.current;
+            if (!container) return;
+            
+            const containerWidth = container.clientWidth;
+            let totalWidth = 0;
+            const tempVisible: string[] = [];
+
+            setMoreCount(0);
+            
+            // Create a temporary span to measure each department's width
+            const tempSpan = document.createElement('span');
+            tempSpan.className = "bg-blue-200 text-gray-900 text-xs rounded px-1 py-0.5 mt-2 mr-2";
+            tempSpan.style.visibility = 'hidden';
+            tempSpan.style.position = 'absolute';
+            document.body.appendChild(tempSpan);
+            
+            for (let i = 0; i < listing.departments.length; i++) {
+                tempSpan.textContent = listing.departments[i];
+                const width = tempSpan.getBoundingClientRect().width + 8; // 8px for margin
+                
+                if (totalWidth + width <= containerWidth) {
+                    tempVisible.push(listing.departments[i]);
+                    totalWidth += width;
+                } else {
+                    setMoreCount(listing.departments.length - i);
+                    break;
+                }
+            }
+
+            if(tempVisible.length !== listing.departments.length) {
+                // Measure the "+x more" bubble
+                tempSpan.textContent = `+${listing.departments.length - tempVisible.length} more`;
+                const moreWidth = tempSpan.getBoundingClientRect().width + 8; // 8px for margin
+
+                // Check if the "+x more" bubble fits
+                if (totalWidth + moreWidth > containerWidth) {
+                    // Remove the last department to make space for the "+x more" bubble
+                    tempVisible.pop();
+                    setMoreCount(listing.departments.length - tempVisible.length);
+                }
+                }
+            
+            document.body.removeChild(tempSpan);
+            setVisibleDepartments(tempVisible);
+        };
+        
+        calculateVisibleDepartments();
+        // Re-calculate on window resize
+        window.addEventListener('resize', calculateVisibleDepartments);
+        return () => window.removeEventListener('resize', calculateVisibleDepartments);
+    }, [listing]);
+
+    const toggleFavorite = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        updateFavorite(listing.id, !isFavorite);
+    }
+
+    const handleListingClick = () => {
+        openModal(listing);
+    }
+
+    if (!listing) {
+        return null;
+    }
+
+    return (
+        <div className="mb-4 relative">
+            <div
+                key={listing.id}
+                className="flex relative z-10"
+            >
+                <div 
+                    className={`${getHiringStatusColor()} cursor-pointer rounded-l flex-shrink-0 my-2 relative`} 
+                    style={{ width: '6px' }}
+                    onMouseEnter={() => setShowTooltip(true)}
+                    onMouseLeave={() => setShowTooltip(false)}
+                >
+                    {showTooltip && (
+                        <div className={`${getHiringStatusColor()} absolute top-1/2 left-4 -translate-y-1/2 text-white text-xs rounded-full py-1 px-2 z-10 whitespace-nowrap shadow`}>
+                            {getHiringStatusText()}
+                        </div>
+                    )}
+                </div>
+                <div className="p-4 flex-grow grid grid-cols-3 md:grid-cols-12 cursor-pointer bg-white hover:bg-gray-100 border border-gray-300 rounded shadow" onClick={handleListingClick}>
+                    {/* First Column */}
+                    <div className="col-span-2 md:col-span-4">
+                        <p className={`text-lg font-semibold mb-3`} style={{ lineHeight: '1.2rem', height: '1.2rem', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{listing.title}</p>
+                        <p className={`text-sm text-gray-700`} style={{ overflow: 'hidden', display: '-webkit-box', WebkitLineClamp: 1, WebkitBoxOrient: 'vertical' }}>
+                            <strong>Professors:</strong> {[`${listing.ownerFirstName} ${listing.ownerLastName}`, ...listing.professorNames].join(', ')}
+                        </p>
+                        {/* list all departments in blue bubbles*/}
+                        <div ref={departmentsContainerRef} className="flex overflow-hidden" style={{ whiteSpace: 'nowrap' }}>
+                            {visibleDepartments.length > 0 ? (
+                                <>
+                                    {visibleDepartments.map((department) => (
+                                        <span
+                                            key={department}
+                                            className={`${Object.keys(departmentCategories).includes(department) ? departmentColors[departmentCategories[department as keyof typeof departmentCategories]] : "bg-gray-200"} text-gray-900 text-xs rounded px-1 py-0.5 mt-3 mr-2`}
+                                            style={{ display: 'inline-block', whiteSpace: 'nowrap' }}
+                                        >
+                                            {department}
+                                        </span>
+                                    ))}
+                                    {moreCount > 0 && (
+                                        <span
+                                            className={`bg-gray-200 text-gray-900 text-xs rounded px-1 py-0.5 mt-3`}
+                                            style={{ display: 'inline-block', whiteSpace: 'nowrap' }}
+                                        >
+                                            +{moreCount} more
+                                        </span>
+                                    )}
+                                </>
+                            ) : (
+                                <div className="mt-3 flex">
+                                    <span
+                                        className={`invisible bg-gray-200 text-gray-900 text-xs rounded px-1 py-0.5 mr-2`}
+                                        style={{ display: 'inline-block' }}
+                                    >
+                                        placeholder
+                                    </span>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+
+                    {/* Second Column */}
+                    <div className="col-span-6 hidden md:flex align-middle">
+                        {/* Vertical Line */}
+                        <div className={`flex-shrink-0 border-l border-gray-300 mx-4`} />
+                        <p className={`flex-grow text-gray-800 text-sm overflow-hidden overflow-ellipsis`} style={{ display: '-webkit-box', WebkitLineClamp: 4, WebkitBoxOrient: 'vertical' }}>
+                            {listing.description}
+                        </p>
+                    </div>
+
+                    {/* Third Column */}
+                    <div className="flex flex-col col-span-1 md:col-span-2 items-end">
+                        <div>
+                            {listing.websites && listing.websites.length > 0 && (
+                                <a
+                                    href={listing.websites[0]}
+                                    className = 'mr-1'
+                                    onClick={(e) => e.stopPropagation()}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <button className="p-1 rounded-full hover:bg-gray-200">
+                                        <img
+                                            src="/assets/icons/link.svg"
+                                            alt="Lab Website"
+                                            className={`w-5 h-5`}
+                                        />
+                                    </button>
+                                </a>
+                            )}
+                            <a onClick={toggleFavorite} className="inline-block">
+                                <button 
+                                    className="p-1 hover:bg-gray-200 rounded-full"
+                                    aria-label={isFavorite ? "Remove from favorites" : "Add to favorites"}
+                                >
+                                    <svg 
+                                        xmlns="http://www.w3.org/2000/svg" 
+                                        width="20" 
+                                        height="20" 
+                                        viewBox="0 0 24 24" 
+                                        className={`transition-colors`}
+                                        fill={isFavorite ? "#FFDA7B" : "none"} 
+                                        stroke={isFavorite ? "#F0C04A" : "currentColor"} 
+                                        strokeWidth="1.5" 
+                                        strokeLinecap="round" 
+                                        strokeLinejoin="round"
+                                    >
+                                        <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+                                    </svg>
+                                </button>
+                            </a>
+                        </div>
+                        <div className="flex-grow" />
+                        <p className={`text-sm text-gray-700`} style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', maxWidth: '100%' }}>
+                            {new Date(listing.updatedAt).toLocaleDateString()}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ListingCard;
---
+++ client/src/components/home/ListingsCardList.tsx
@@ -0,0 +1,173 @@
+import * as React from 'react';
+import ListingsModal from '../home/ListingModal';
+import ListingCard from './ListingCard';
+import PulseLoader from "react-spinners/PulseLoader";
+import { NewListing } from '../../types/types';
+
+type Order = 'asc' | 'desc';
+
+type ListingsCardListProps = {
+  loading: Boolean;
+  searchExhausted: Boolean;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  listings: NewListing[];
+  sortableKeys: string[];
+  setSortBy: (sortBy: string) => void;
+  setSortOrder: (sortOrder: number) => void;
+  favListingsIds: string[];
+  updateFavorite: (listingId: string, favorite: boolean) => void;
+};
+
+export default function ListingsCardList({ loading, searchExhausted, setPage, listings, sortableKeys, setSortBy, setSortOrder, favListingsIds, updateFavorite }: ListingsCardListProps) {
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderIndex, setOrderIndex] = React.useState(0);
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const [selectedListingId, setSelectedListingId] = React.useState<string | null>(null);
+
+  // Reference for results bottom detector
+  const bottomObserverRef = React.useRef<HTMLDivElement>(null);
+  
+  // Set up intersection observer for infinite scrolling
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        // If bottom element is visible, loading is false, and we haven't exhausted search results
+        if (entry.isIntersecting && !loading && !searchExhausted) {
+          setPage(prevPage => prevPage + 1);
+        }
+      },
+      { threshold: 1.0 }
+    );
+    
+    const currentObserver = bottomObserverRef.current;
+    if (currentObserver) {
+      observer.observe(currentObserver);
+    }
+    
+    return () => {
+      if (currentObserver) {
+        observer.unobserve(currentObserver);
+      }
+    };
+  }, [loading, searchExhausted, setPage]);
+
+  const buttonTranslations: Record<string, string[]> = {
+    'default': ['Best Match', '', ''],
+    'updatedAt': ['Last Updated', 'Newest', 'Oldest'],
+    'ownerLastName': ['Last Name', 'A-Z', 'Z-A'],
+    'ownerFirstName': ['First Name', 'A-Z', 'Z-A'],
+    'title': ['Lab Title', 'A-Z', 'Z-A']
+  }
+
+  React.useEffect(() => {
+    setSortOrder(order === 'asc' ? 1 : -1);
+  }, [order])
+
+  React.useEffect(() => {
+    setSortBy(sortableKeys[orderIndex]);
+  }, [orderIndex])
+
+  const handleToggleSortKey = () => {
+    setOrderIndex((prevIndex) => (prevIndex + 1) % sortableKeys.length);
+  }
+
+  const handleToggleOrder = () => {
+    setOrder((prevOrder) => prevOrder === 'asc' ? 'desc' : 'asc');
+  }
+
+  const openModalForListing = (listing: NewListing) => {
+    setSelectedListingId(listing.id);
+    setModalOpen(true);
+  };
+
+  // Scroll to top button
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <div className="flex flex-col items-center p-4 relative">
+      
+      {/* Modal */}
+        {!loading && selectedListingId !== null && (
+            <ListingsModal
+              listing={listings.find((l) => l.id === selectedListingId)!}
+              onClose={() => {
+                setModalOpen(false);
+                setSelectedListingId(null);
+              }}
+              isOpen={modalOpen}
+              favListingsIds={favListingsIds}
+              updateFavorite={updateFavorite}
+            />
+      )}
+
+      {/* Sorting Buttons */}
+      <div className="mb-4 flex justify-between w-full" style={{ maxWidth: '80%' }}>
+        <button
+          onClick={() => {
+            if (!loading) {
+              handleToggleSortKey();
+            }
+          }}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          Sort: {buttonTranslations[sortableKeys[orderIndex]][0]}
+        </button>
+        {sortableKeys[orderIndex] !== 'default' && (
+          <button
+            onClick={() => {
+              if (!loading) {
+                handleToggleOrder();
+              }
+            }}
+            className="px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            {order === 'asc' ? buttonTranslations[sortableKeys[orderIndex]][1] : buttonTranslations[sortableKeys[orderIndex]][2]}
+          </button>
+        )}
+      </div>
+
+      
+      {/* List of Cards (Rows) with Pulse Loader conditionally below*/}
+      <div className="w-full" style={{ maxWidth: '80%' }}>
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            favListingsIds={favListingsIds}
+            listing={listing}
+            updateFavorite={updateFavorite}
+            openModal={openModalForListing}
+          />
+        ))}
+        
+        {/* Detects bottom of the list */}
+        {!searchExhausted && (
+          <div 
+            ref={bottomObserverRef}
+            className="h-10 w-full"
+          />
+        )}
+      </div>
+
+      {loading && (
+        <div className="flex justify-center items-center mt-4">
+          <PulseLoader color="#3b82f6" size={15} />
+        </div>
+      )}
+
+      {/* Scroll to Top Button */}
+      <button
+        onClick={() => {
+          if (!loading) {
+            scrollToTop();
+          }
+        }}
+        className="fixed bottom-6 right-6 bg-blue-500 text-white p-3 rounded-full shadow-lg hover:bg-blue-700 transition"
+      >
+        ⬆️
+      </button>
+    </div>
+  );
+}
