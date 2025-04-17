c9b8f89ce17d1f38782318ff0ea4f470e9799d9b
Ryan Fernandes <ryan@fernandes.us>
2025-04-17 10:22:16 -0400

    Home button reloads page if on home

--- client/src/components/HomeButton.tsx
+++ client/src/components/HomeButton.tsx
@@ -1,11 +1,22 @@
 import Button from "@mui/material/Button";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const HomeButton = () => {
+    const location = useLocation();
+    
+    const handleClick = (event: React.MouseEvent) => {
+        // If already on the home page, reload the page
+        if (location.pathname === '/') {
+            event.preventDefault();
+            window.location.reload();
+        }
+    };
+
     return(
         <Button
             component={Link}
             to="/"
+            onClick={handleClick}
         >
             <img src="/assets/logos/ylabs-temp.png" alt="ylabs-logo" style={{width: '65px', height: '26px'}} />
         </Button>
